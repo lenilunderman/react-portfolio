7779805767dcbd29fd291dd851c19f7602be82ec
lenilunderman <lenilunderman@gmail.com>
2021-05-14 00:27:18 -0400

    Build app tours

---
+++ 02-tours/build/precache-manifest.9e72faffddc0abe10e0e79456aa1bd8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7aa440a29dbfc282d6e2496dc0b6fcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dacb7f1356a54c013d56",
+    "url": "/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dacb7f1356a54c013d56",
+    "url": "/static/js/main.929646d5.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
