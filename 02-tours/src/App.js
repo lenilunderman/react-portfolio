539284e1eebcc4f426990b4928f0d67966ae4cde
lenilunderman <lenilunderman@gmail.com>
2021-05-14 00:20:34 -0400

    Project Tour, react use state, use effect and short circuit.

--- 02-tours/src/App.js
+++ 02-tours/src/App.js
@@ -5,7 +5,60 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([])
+
+  // remove tour
+  const removeTour = (id) => {
+    // check if matches and return only the one it doesnt match
+    const newTour = tours.filter((item) => item.id !== id)
+    setTours(newTour)
+  }
+
+  // fetch the data
+  const fetchTours = async () => {
+    setLoading(true)
+    try {
+      const res = await fetch(url)
+      const tours = await res.json()
+      setLoading(false)
+      setTours(tours)
+      console.log(tours)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  // effects
+  useEffect(() => {
+    fetchTours()
+  }, [])
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No tours left</h2>
+          <button onClick={fetchTours} className="btn">
+            {' '}
+            Refresh Page
+          </button>
+        </div>
+      </main>
+    )
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  )
 }
 
 export default App
