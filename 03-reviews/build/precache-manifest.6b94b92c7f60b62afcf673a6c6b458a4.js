4458e4b3692346cb5c189d4d3912184f4e292105
lenilunderman <lenilunderman@gmail.com>
2021-05-23 14:47:09 -0400

    Final built of project Our reviews.

---
+++ 03-reviews/build/precache-manifest.6b94b92c7f60b62afcf673a6c6b458a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b39e155afa72202e5455e3395bbbe7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc6ebefee71a4b31f0d3",
+    "url": "/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc6ebefee71a4b31f0d3",
+    "url": "/static/js/main.e258c5d0.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
